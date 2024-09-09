79123f588bdca9d8c8f4f77b22cd1b47c7f2b1a2
F6320854 Lucas Aguiar Vita <lucas.vita@bb.com.br>
2024-09-09 14:00:09 -0300

    Commite teste

--- scripts/list_installed_programs.sh
+++ scripts/list_installed_programs.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-if command -v brew &> /dev/null; then
-  brew list --formula
-elif command -v dpkg &> /dev/null; then
-  dpkg --list
+
+# Identificar o sistema operacional
+OS="$(uname)"
+
+# Verificar os softwares instalados e suas versões
+if [ "$OS" = "Linux" ]; then
+    echo "Nome do Software,Versão"
+    dpkg-query -W -f='${binary:Package},${Version}\n'
+elif [ "$OS" = "Darwin" ]; then
+    echo "Nome do Software,Versão"
+    brew list --versions | while read cask; do echo $cask | awk '{print $1 "," $2}'; done
 else
-  echo "Não foi possível determinar o gerenciador de pacotes."
+    echo "Sistema Operacional não suportado."
 fi
